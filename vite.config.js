7a407b49b69acb8084d127daff210294a177a6d1
lucas <lucassborgess600@gmail.com>
2025-11-21 00:07:02 -0300

    Primeiro deploy portifolio

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // ATENÇÃO: Coloque aqui o nome exato do seu repositório entre barras
+  base: '/meu-portifolio/', 
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
 })
